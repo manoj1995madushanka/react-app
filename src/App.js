dbf1adc4f28e383d06a98ee2b30411a780e6a997
unknown <=>
2023-09-03 04:25:30 -0700

    react navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import {Link} from "react-router-dom";
 
 export function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/about">Abount</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>My Web</h1>
     </div>
   );
@@ -11,6 +16,11 @@ export function Home() {
 export function About() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">Abount</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>About us</h1>
     </div>
   );
@@ -19,6 +29,11 @@ export function About() {
 export function Contact() {
   return (
     <div>
+       <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">Abount</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>Contact Us</h1>
     </div>
   );
