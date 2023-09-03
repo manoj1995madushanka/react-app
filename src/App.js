1a887f194d8320881411c82bc91eb8b6140803a1
unknown <=>
2023-09-03 02:31:04 -0700

    form control

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
-import {useReducer } from 'react';
+import {useRef } from 'react';
 
-function App({library}) {
+function App() {
+  const txtTitle = useRef();
+  const hexColor = useRef();
 
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  console.log(txtTitle);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title} : ${color}`);
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  };
+ 
   return (
-    <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
-    </div>
-  );
+    <form onSubmit={submit}>
+      <input ref={txtTitle} type="text" placeholder="color title..."/>
+      <input ref={hexColor} type='color'/>
+      <button>ADD</button>
+    </form>
+  )
 }
 
 export default App;
